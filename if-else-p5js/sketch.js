d75645e570a4ef2fcdda1cb625620418e74f447c
Goncalomorais0506 <161716005+Goncalomorais0506@users.noreply.github.com>
2024-05-31 22:52:06 +0100

    Add files via upload

---
+++ if-else-p5js/sketch.js
@@ -0,0 +1,28 @@
+function setup(){
+    createCanvas(400,400);
+    background(255);
+  }
+  
+  function draw(){
+    background(255);
+    stroke(0);
+    strokeWeight(3);
+  
+    if(mouseX < width/2) {
+      if(mouseY < height/2){
+        fill(255,0,0);
+      }
+      else{
+        fill(0,255,0);
+      }
+    }
+    else {
+      if(mouseY < height/2){
+        fill(0,0,255);
+      }
+      else{
+        fill(255);
+      }
+    }
+    circle(width/2, height/2, height/4);
+  }
